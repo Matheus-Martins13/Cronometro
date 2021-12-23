39dd363554b10132eea37692a925ee793dc777b3
Matheus Martins <matheusmmartins8@gmail.com>
2021-12-23 17:09:22 -0300

    Página de cronômetro finalizada

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+function scope() {
+
+    function getTimeFromSeconds(second) {
+        let time = new Date(second * 1000);
+        return time.toLocaleTimeString('pt-BR', {
+            timeZone: 'UTC'
+        });
+    }
+
+    const clock = document.querySelector('.clock');
+    let second = 0;
+    let timer;
+
+    function start () {
+       timer = setInterval(function(){
+           second++
+           clock.innerHTML = getTimeFromSeconds(second);
+       }, 1000);
+    }
+
+    function reset () {
+        clearInterval(timer);
+        second = 0
+        clock.innerHTML = getTimeFromSeconds(second);
+    }
+
+    document.addEventListener('click', function (e) {
+        const el = e.target;
+
+        if (el.classList.contains('start')){
+            clearInterval(timer);
+            start();
+            clock.style.color = 'blue';
+        }
+        if (el.classList.contains('stop')){
+            clearInterval(timer);
+            clock.style.color = 'red';
+        }
+        if (el.classList.contains('reset')){
+            reset();
+            clock.style.color = 'black';
+        }
+    })
+
+}
+
+scope();
